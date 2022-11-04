334d8d545571a1a981bd5cc52ddd367a8ab6164f
Akkrapol Kanpong <aukkrapolarm@gmail.com>
2022-11-05 03:21:28 +0700

    HBD

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./giftbox.png";
+import "./App.css";
 
 function App() {
+
+  const today  = new Date()
+
+  const [modal, setModal] = useState(false);
+  const [x, setx] = useState(false);
+  const [btc, setBtc] = useState(0)
+
+  const lucky = () => {
+    const luck = Math.floor(Math.random() * 3)
+    if(luck === 0) {
+      setBtc(100)
+    }
+    if(luck === 1) {
+      setBtc(300)
+    }
+    if(luck === 2) {
+      setBtc(500)
+    }
+    setx(!x)
+    console.log(btc)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link">สุ่ม Lucky Box</p>
+        <button onClick={lucky} className="App-Button-Luck">
+          สุ่ม
+        </button>
+        <button onClick={() => setModal(!modal)} className="App-Button-Info">
+          รายละเอียดการเล่น
+        </button>
+        <div style={{ display: modal ? `flex` : `none` }} className="modal">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="App-Button-Close"
+                onClick={() => setModal(false)}
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+              <h2 className="modal-title">รายละเอียดการเล่น</h2>
+            </div>
+            <div className="modal-body">
+              <p>1. กดสุ่มได้เพียง 1 นั้น</p>
+              <p>
+                2. หลังจากรับรับ Link คุณต้องกดสุ่มไม่เกินเวลาที่ผู้สร้างกำหนด
+              </p>
+              <p>3. หลังจากสุ่มเสร็จเเคปหน้าจอส่งผู้สร้าง</p>
+            </div>
+          </div>
+        </div>
+
+        <div style={{ display: x ? `flex` : `none` }} className="App-Modal">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="App-Button-Close"
+                onClick={() => setx(false)}
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+              <h2 className="modal-title">🎂 Happy 🎂 Birthday</h2>
+              <p>{ today.toLocaleDateString("en-US") }</p>
+            </div>
+            <div className="modal-body">
+              <p>
+                Happy Birthday น้าา ขอให้ประสบความสำเร็จทางด้านการเรียน การงาน
+                การเงิน เเละขอให้สุขภาพเเข็งเเรง คิดสิ่งใดขอให้สมปราถนา{" "}
+              </p>
+              <button>{`คุณได้รับ BTC จำนวน ${btc/786050} BTC`}</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
